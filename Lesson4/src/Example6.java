aedf388c898839fd3a150ab59b2b01136699f485
vektorel <vektorel@lab8-ogr>
2017-06-05 20:47:11 +0300

    examples

---
+++ Lesson4/src/Example6.java
@@ -0,0 +1,25 @@
+
+public class Example6 {
+
+	public static void main(String[] args) {
+
+
+		for(int i=3;i>=0;i--) // satýrlarý
+		{
+			
+			for(int j=0;j<i;j++) // sutun iþlemleri
+			{
+				
+				System.out.print(" ");
+				
+			}
+			
+			System.out.println("*");
+			
+		}
+		
+		
+
+	}
+
+}
